5d9e0b472b799a6a70113ee2a9f0bc19aad330df
20B01A04B7 ECE <20b01a04b7@svecw.edu.in>
2022-04-06 09:11:16 +0000

    Update mainfile.py

--- mainfile.py
+++ mainfile.py
@@ -30,3 +30,16 @@ class ColoredLine:
         pygame.display.set_caption("COLORED LINES GAME")
         self.draw_Grid()
         pygame.display.flip()
+
+    def events(self):
+        event_list = get_events()
+        for event in event_list:
+            self.single_Event(event)
+
+    def single_Event(self, event):
+        if event.type == QUIT:
+            self.close = True
+        elif event.type == MOUSEBUTTONUP:
+            self.mouse_pos[0], self.mouse_pos[1] = event.pos
+            self.move_Ball()
+            self.check_Ball()
